16cc99f9a19008fdf39d1f14e4582d8f1fef8f98
larissafiorini <larissa.fiorini@hotmail.com>
2019-08-28 18:15:41 -0300

    starting

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
 
 public class Vigenere {
-	
-	public Vigenere() {}
-	
+
+	// indice de coincidencia para o portugues (probabilidade de dois elementos
+	// aleatórios de x serem identicos)
+	private static final double index_of_coincidence = 0.072723;
+	private static final int min_key_length = 2;
+	private static final int max_key_length = 25;
+
+	public Vigenere() {
+	}
+
 	public static int findKeyLength(String texto_cifrado) {
+		// indice de coincidencia
 		return 0;
 	}
 }
