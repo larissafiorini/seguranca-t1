6ddcf42899f2bf4ec49cff39f8658311088b8257
larissafiorini <larissa.fiorini@hotmail.com>
2019-09-01 22:15:33 -0300

    organizando método

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Vigenere {
 
@@ -30,8 +31,7 @@ public class Vigenere {
 		return tamanho;
 	}
 
-	// encontrar caracteres da chave
-	public static void analiseFrequencia(int tamanho_chave, String texto_cifrado) {
+	public static ArrayList<String> cifrasCesar(int tamanho_chave, String texto_cifrado) {
 
 		// monta cada linha com presenca em um caracter da chave, avancando pelo tamanho
 		// da chave em cada index
@@ -56,60 +56,103 @@ public class Vigenere {
 			System.out.println(linha);
 		}
 
-		// verifica quais caracteres aparecem mais em cada posicao possivel da chave
-		// testando pro primeiro caracter da chave primeiro
-		String subtext = linhas.get(3);
-
-		// Loop para calcular a frequência
-		// ASCII:
-		// 97 = a
-		// 122 = z
-		int frequencias[] = new int[26];
-		int c = 0;
-		for (int j = 97; j <= 122; j++) {
-			int frequencia = 0;
-
-			for (int k = 0; k < subtext.length(); k++) {
-				if (subtext.charAt(k) == (char) j)
-					frequencia++;
-			}
-			System.out.println(" asc[" + j + "] = " + frequencia);
-			frequencias[c] = frequencia;
-			c++;
-		}
-		int maior_frequencia = 0;
-		// 26=numero de letras do alfabeto
-		for (int i = 0; i < 26; i++) {
-			if (frequencias[i] > frequencias[maior_frequencia])
-				maior_frequencia = i;
-		}
-		System.out.println("maior_frequencia: " + maior_frequencia);
-		// letra mais frequente portugues: a
-		System.out.println((char) (maior_frequencia + 97) + " eh o a");
-		System.out.println(maior_frequencia + 97);
-		System.out.println((char) (maior_frequencia + 97));
-
-		// each one needs 26 tries to decrypt, so a key length of 5 gives us a 26^5
-		// cases to try
-		// If, for example, in the first group, the letter “s” is the most repeated one,
-		// we know that “e” is replaced by “s” and hence the key is “s” – “e” = “m”.
-		char mais_repete = (char) (maior_frequencia + 97);
-
-		int letra1 = (int) mais_repete - (int) 'a';
-		if (letra1 < 0)
-			letra1 += 26;
-		System.out.println(letra1);
-		System.out.println(letra1 + 97);
-
-		int letra2 = (int) mais_repete - (int) 'a';
-		letra2 += 97;
-
-		System.out.println("possivel letra da chave: " + (char) letra2);
-
+//		// outra forma
+//		ArrayList<String> resultado = new ArrayList<String>();
+//		StringBuilder[] sbb = new StringBuilder[tamanho_chave];
+//		
+//		for (int i = 0; i < tamanho_chave; i++)
+//			sbb[i] = new StringBuilder();
+//		
+//		for (int i = 0; i < texto_cifrado.length(); i++)
+//			sbb[i % tamanho_chave].append(texto_cifrado.charAt(i));
+//		
+//		for (StringBuilder s : sbb) {
+////			System.out.println(s.toString());
+//			resultado.add(s.toString());
+//		}
+		return linhas;
 	}
 
-	
-	// sabendo qual a chave, decifra vigenere pelas cifras de cesar simples agora conhecidas
+	// encontrar caracteres da chave
+	public static void analiseFrequencia(int tamanho_chave, String texto_cifrado) {
+		ArrayList<String> linhas = cifrasCesar(tamanho_chave, texto_cifrado);
+
+		// verifica quais caracteres aparecem mais em cada posicao possivel da chave
+		// testando pro primeiro caracter da chave primeiro
+		// String subtext = linhas.get(1);
+		StringBuilder key_final = new StringBuilder();
+		StringBuilder key_final2 = new StringBuilder();
+
+		for (String linha : linhas) {
+			String subtext = linha;
+
+			// Loop para calcular a frequência
+			// ASCII:
+			// 97 = a
+			// 122 = z
+			int frequencias[] = new int[26];
+			int c = 0;
+			for (int j = 97; j <= 122; j++) {
+				int frequencia = 0;
+
+				for (int k = 0; k < subtext.length(); k++) {
+					if (subtext.charAt(k) == (char) j)
+						frequencia++;
+				}
+				System.out.println(" asc[" + j + "] = " + frequencia);
+				frequencias[c] = frequencia;
+				c++;
+			}
+
+			int maior_frequencia = 0;
+			// 26=numero de letras do alfabeto
+			for (int i = 0; i < 26; i++) {
+				if (frequencias[i] > frequencias[maior_frequencia])
+					maior_frequencia = i;
+			}
+			System.out.println("maior_frequencia: " + maior_frequencia);
+			// letra mais frequente portugues: a
+			System.out.println((char) (maior_frequencia + 97) + " eh o a");
+			System.out.println(maior_frequencia + 97);
+			System.out.println((char) (maior_frequencia + 97));
+
+			// 'E' is the maxChar
+			// Console.WriteLine(string.Format("E is probably: {0} - {1}", maxChar,
+			// maxFreq));
+			int testa = (char) 'a' - (char) (maior_frequencia + 97);
+			System.out.println("testa:" + testa);
+
+			// Console.WriteLine(string.Format("Key would be: {0}", (char) (maxChar - 'e' +
+			// 'A')));
+			int keyyy = ((char) (maior_frequencia + 97) - (char) 'a') + 97;
+			System.out.println("keyyy:" + keyyy);
+			System.out.println("keyyy:" + (char) keyyy);
+			key_final.append((char) keyyy);
+
+			// each one needs 26 tries to decrypt, so a key length of 5 gives us a 26^5
+			// cases to try
+			// If, for example, in the first group, the letter “s” is the most repeated one,
+			// we know that “e” is replaced by “s” and hence the key is “s” – “e” = “m”.
+			char mais_repete = (char) (maior_frequencia + 97);
+
+			int letra1 = (int) mais_repete - (int) 'a';
+			if (letra1 < 0)
+				letra1 += 26;
+			System.out.println(letra1);
+			System.out.println(letra1 + 97);
+
+			int letra2 = (int) mais_repete - (int) 'a';
+			letra2 += 97;
+
+			System.out.println("possivel letra da chave: " + (char) letra2);
+			key_final2.append((char) letra2);
+		}
+		System.out.println("KEYFINAL 1: " + key_final.toString());
+		System.out.println("KEYFINAL 2: " + key_final2.toString());
+	}
+
+	// sabendo qual a chave, decifra vigenere pelas cifras de cesar simples agora
+	// conhecidas
 	public static void decifra(String chave, String texto_cifrado) {
 		int tamanho_chave = chave.length();
 		int d;
